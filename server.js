cd488dea6714dac1172768493a0d31b9612ba6eb
jheckjr <joel.heckjr@gmail.com>
2016-10-22 18:53:52 -0400

    Add Express structure

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var routes = require('./app/routes/index.js');
+
+var app = express();
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
+routes(app);
+
+app.listen(process.env.PORT || 8080, function() {
+	console.log('Node.js listening...');
+});
